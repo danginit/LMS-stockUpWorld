f5e5134f027cdd1aa935a40ce117814f2c0a3bab
danginit <32682428+danginit@users.noreply.github.com>
2020-08-11 12:48:37 +0530

    Update my_course.php

--- my_course.php
+++ my_course.php
@@ -27,7 +27,62 @@ include('css/sidebar.php');
 		</div><!--/.row-->
 
 		<div class="panel panel-container">
-			
+			<div class="row" style="padding-left:25px;">
+				<table class="table" id="dataTable" width="100%" cellspacing="0">
+		      <thead>
+		        <tr>
+							<th>Course Name</th>
+							<th>Enroll Date</th>
+							<th>End Date</th>
+							<th>Action</th>
+		        </tr>
+		      </thead>
+							<?php
+							 $connection = mysqli_connect("localhost","root","","signup");
+							 $query = "SELECT course_name,enroll_date,end_date FROM courses";
+							 $query_run = mysqli_query($connection, $query);
+              ?>
+
+    <tbody >
+      <?php
+      if(mysqli_num_rows($query_run)>0)
+      {
+        while($row = mysqli_fetch_assoc($query_run))
+        {
+					?>
+					<tr>
+						<td > <?php echo $row['course_name']; ?></td>
+						<td> <?php echo $row['enroll_date']; ?></td>
+						<td> <?php echo $row['end_date']; ?></td>
+						<td>
+								<form action="register_edit.php" method="post">
+										<input type="hidden" name="edit_id" value="<?php echo $row['id']; ?>">
+										<button type="submit" name="edit_btn" class="btn btn-success">Go to Course</button>
+								</form>
+						</td>
+					<tr>
+        <?php
+			   }
+      }
+			else{
+					echo 'No Record found';
+				}
+				?>
+				<?php
+				if(isset($_SESSION['success']) && $_SESSION['success'] !='')
+				{
+					echo '<h2> ' .$_SESSION['success'].' </h2>';
+					unset($_SESSION['success']);
+				}
+				if(isset($_SESSION['status']) && $_SESSION['status'] !='')
+				{
+					echo '<h2> ' .$_SESSION['status'].' </h2>';
+					unset($_SESSION['status']);
+				}
+				?>
+    </tbody>
+  	</table>
+	</div>
 		</div>
 </body>
 </html>
